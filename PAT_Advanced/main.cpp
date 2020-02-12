c1c1cf324d0e84945122ff7147872a65995a59bb
JQY <JQYisBest@foxmail.com>
2020-02-12 15:58:20 +0800

    这个OAC真是毫无成就感。。。最简单的一题了应该是。

--- PAT_Advanced/main.cpp
+++ PAT_Advanced/main.cpp
@@ -1,8 +1,8 @@
 #include "common.h"
-#include "resolve1010.h"
+#include "resolve1011.h"
 
 int main() {
-	int result_code = resolve1010::resolve();
+	int result_code = resolve1011::resolve();
 	cout << "\n";
 	system("pause");
 	return result_code;
---
+++ PAT_Advanced/resolve1011.h
@@ -0,0 +1,4 @@
+#include "common.h"
+namespace resolve1011 {
+	int resolve();
+}
