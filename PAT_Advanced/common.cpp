848df94e23c90f1a5da39c258c2d7c817576aa0d
JQY <JQYisBest@foxmail.com>
2020-02-21 10:20:50 +0800

    加入一些工具函数

--- PAT_Advanced/common.cpp
+++ PAT_Advanced/common.cpp
@@ -33,5 +33,85 @@ namespace common {
 			}
 		}
 	}
+
+	bool is_prime(long long n)
+	{
+		if (n <= 1) return false;
+		for (long long j = 2; j <= sqrt(n); ++j) {
+			if (n%j == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool sort_by_alphabetical_asc(string a, string b)
+	{
+		auto min_size = a.size() <= b.size() ? a.size() : b.size();
+		for (auto i = 0; i <min_size; ++i)
+		{
+			if (a[i]!=b[i])
+			{
+				return a[i] < b[i];
+			}
+		}
+		return min_size == a.size();
+	}
+
+	bool sort_by_alphabetical_desc(string a, string b)
+	{
+		auto min_size = a.size() >= b.size() ? a.size() : b.size();
+		for (auto i = 0; i <min_size; ++i)
+		{
+			if (a[i] != b[i])
+			{
+				return a[i] > b[i];
+			}
+		}
+		return min_size ==b.size();
+	}
+
+	int symbol_to_int(char digit)
+	{
+		if (digit >= '0' && digit <= '9')
+			return digit - '0';
+		return digit - 'a' + 10;
+	}
+
+	long long change_to_radix_ten(string digits, long long radix)
+	{
+		long long result = 0;
+		for (int i = digits.size() - 1, j = 0; i >= 0; --i)
+		{
+			long long int_value = symbol_to_int(digits[i]);
+			if (int_value != -1)
+			{
+				result += int_value*pow(radix, j);
+				++j;
+			}
+		}
+		return result;
+	}
+
+	string change_from_radix_ten(long long digits_in_radix_ten, long long target_radix)
+	{
+		//除基取余法
+		string ans = "";
+		//使用do{}while（）以防止输入为0的情况
+		do {
+			int t = digits_in_radix_ten%target_radix;
+			if (t >= 0 && t <= 9)
+			{
+				ans += t + '0';
+			}
+			else
+			{
+				ans += t - 10 + 'a';
+			}
+			digits_in_radix_ten /= target_radix;
+		} while (digits_in_radix_ten != 0);
+		reverse(ans.begin(),ans.end());	
+		return ans;
+	}
 	
 }
--- PAT_Advanced/common.h
+++ PAT_Advanced/common.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <algorithm> 
 #include <vector>
+#include <set>
+#include <map>
 #include <iomanip>
 using namespace std;
 namespace common {
@@ -10,4 +12,17 @@ namespace common {
 	void print_array(int** a, int m, int n);
 	void fill_array(int* a, int n, int val);
 	void fill_array(int** a, int m, int n, int val);
+	bool is_prime(long long n);
+	/**
+	    按字符升序排序的比较函数
+	*/
+	bool sort_by_alphabetical_asc(string a, string b);
+	/**
+	    按字符降序排序的比较函数
+	*/
+	bool sort_by_alphabetical_desc(string a, string b);
+	int symbol_to_int(char digit);
+	long long change_to_radix_ten(string digits, long long radix);
+	string change_from_radix_ten(long long digits_in_radix_ten, long long target_radix);
+	
 }
