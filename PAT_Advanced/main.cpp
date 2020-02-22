af43a996b81c39b86f18c7b0ad69f6135c72c096
JQY <JQYisBest@foxmail.com>
2020-02-22 16:52:55 +0800

    AC，可惜了。想当然的觉得即使进制再大只要数位一致那加进去的字符就能用==判断是否相等。。

--- PAT_Advanced/main.cpp
+++ PAT_Advanced/main.cpp
@@ -1,8 +1,8 @@
 #include "common.h"
-#include "resolve1018.h"
+#include "resolve1019.h"
 
 int main() {
-	int result_code = resolve1018::resolve();
+	int result_code = resolve1019::resolve();
 	cout << "\n";
 	system("pause");
 	return result_code;
---
+++ PAT_Advanced/resolve1019.h
@@ -0,0 +1,4 @@
+#include "common.h"
+namespace resolve1019 {
+	int resolve();
+}
