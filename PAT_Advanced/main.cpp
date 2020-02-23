3e2e97696fa112ae4320694c43f9857edf21e957
JQY <JQYisBest@foxmail.com>
2020-02-23 16:08:55 +0800

    OAC，后序中序转层序或先序，终于将之前做题时的思路代码化了

--- PAT_Advanced/main.cpp
+++ PAT_Advanced/main.cpp
@@ -1,8 +1,8 @@
 #include "common.h"
-#include "resolve1019.h"
+#include "resolve1020.h"
 
 int main() {
-	int result_code = resolve1019::resolve();
+	int result_code = resolve1020::resolve();
 	cout << "\n";
 	system("pause");
 	return result_code;
