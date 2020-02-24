0cb01e8bc7ea937de713b75925cb4fc514b0e902
JQY <JQYisBest@foxmail.com>
2020-02-24 16:51:21 +0800

    艰难AC，递归中尽量不要拷贝传递STL！容易超时！

--- PAT_Advanced/resolve1021.cpp
+++ PAT_Advanced/resolve1021.cpp
@@ -62,6 +62,173 @@
 	5
 	答案正确
 	4 ms	432 KB
+
+	！！！我的天，居然真的是因为stl复制太多次导致超时，将vector<vector<int>> nodes提为全局变量就过了。爷吐了。
+	0
+	答案正确
+	3 ms	404 KB
+	1
+	答案正确
+	4 ms	300 KB
+	2
+	答案正确
+	5 ms	384 KB
+	3
+	答案正确
+	35 ms	1536 KB
+	4
+	答案正确
+	3 ms	424 KB
+	5
+	答案正确
+	3 ms	364 KB
+
+	总结一下AC的关键点：
+	1、dfs次数尽可能少
+	2、stl容器尽可能少的拷贝传递（重中之重！！！本来我自己的想法也是能过的/(ㄒoㄒ)/~~）
+
+	测试第一版暴力计算每一个节点的想法，发现在将vector<vector<int>> nodes提为全局变量后时间大幅减少，测试点3从超时变成内存超限
+
+	将每一个节点作为根节点的循环里的visited数组提出循环外后AC了，原来是忘了释放内存，java写习惯了以为会GC。/(ㄒoㄒ)/~~ 
+	0
+	答案正确
+	4 ms	424 KB
+	1
+	答案正确
+	4 ms	356 KB
+	2
+	答案正确
+	5 ms	424 KB
+	3
+	答案正确
+	1183 ms	1092 KB
+	4
+	答案正确
+	4 ms	424 KB
+	5
+	答案正确
+	4 ms	424 KB
+	
+	以下是第一版的代码，没有借鉴任何人纯粹自己的想法
+
+	#include <iostream>
+	#include <math.h>
+	#include <string>
+	#include <algorithm>
+	#include <list>
+	#include <vector>
+	#include <set>
+	#include <map>
+	#include <iomanip>
+	using namespace std;
+	vector<vector<int>> nodes;
+	void dfs(int root, int * visited)
+	{
+	visited[root] = 1;
+	for (int i = 0; i < nodes[root].size(); ++i)
+	{
+	if (visited[nodes[root][i]] == 0)
+	{
+	dfs(nodes[root][i], visited);
+	}
+	}
+	}
+
+	void dfs(int root,int* visited, int height,int* max_height)
+	{
+	visited[root] = 1;
+	if (height>(*max_height))
+	{
+	*(max_height) = height;
+	}
+	for (int i = 0; i < nodes[root].size(); ++i)
+	{
+	if (visited[nodes[root][i]] == 0)
+	{
+	dfs(nodes[root][i], visited, height+1,max_height);
+	}
+	}
+	}
+
+	int main()
+	{
+	int num_of_nodes = 0;
+	cin >> num_of_nodes;
+	if (1==num_of_nodes)
+	{
+	cout << "1";
+	}
+	else {
+	nodes.resize(num_of_nodes+1);
+	for (size_t i = 0; i < num_of_nodes-1; ++i)
+	{
+	int vertex_1 = 0, vertex_2 = 0;
+	cin >> vertex_1 >> vertex_2;
+	nodes[vertex_1].push_back(vertex_2);
+	nodes[vertex_2].push_back(vertex_1);
+	}
+	//最深的深度
+	int max_height = 0;
+	//最深的树的根
+	vector<int> deepest_roots;
+	int* visited = new int[num_of_nodes + 1];
+	for (size_t root = 1; root < num_of_nodes+1; ++root)
+	{
+	int current_height = 0;
+	fill(visited, visited + num_of_nodes + 1, 0);
+	//走k轮就有k个连通子图
+	size_t k = 0;
+	for (; k < num_of_nodes; ++k)
+	{
+	int current_node = -1;
+	for (size_t j = 1; -1 == current_node&&j < num_of_nodes+1; ++j)
+	{
+	if (visited[j] == 0) {
+	current_node = j;
+	}
+	}
+	if (-1 == current_node)
+	{
+	break;
+	}
+	if (k == 0)
+	{
+	//第一轮，求深度
+	dfs(root, visited, 1, &current_height);
+	}
+	else {
+	//dfs
+	dfs(current_node, visited);
+	}
+	}
+	if (k == 1)
+	{
+	if (max_height<current_height)
+	{
+	max_height = current_height;
+	deepest_roots.clear();
+	deepest_roots.push_back(root);
+	}
+	else if (max_height==current_height)
+	{
+	deepest_roots.push_back(root);
+	}
+	}
+	else
+	{
+	cout << "Error: " << k << " components";
+	return 0;
+	}
+	}
+	sort(deepest_roots.begin(), deepest_roots.end());
+	for (size_t i = 0; i < deepest_roots.size() - 1; ++i)
+	{
+	cout << deepest_roots[i] << endl;
+	}
+	cout << deepest_roots[deepest_roots.size() - 1];
+	}
+	return 0;
+	}
 */
 int resolve1021::resolve()
 {
@@ -72,7 +239,6 @@ int resolve1021::resolve()
 		cout << "1";
 	}
 	else {
-		vector<vector<int>> nodes;
 		nodes.resize(num_of_nodes + 1);
 		for (size_t i = 0; i < num_of_nodes - 1; ++i)
 		{
@@ -82,23 +248,23 @@ int resolve1021::resolve()
 			nodes[vertex_2].push_back(vertex_1);
 		}
 		//最深的深度、连通分量数
-		int max_height=0,k= 0;
+		int max_height = 0, k = 0;
 		int* visited = new int[num_of_nodes + 1];
 		fill(visited, visited + num_of_nodes + 1, 0);
 		//所处高度最深的节点
 		set<int> deepest_nodes;
 		vector<int> current_deepest_nodes;
-		for (int i = 1; i < num_of_nodes+1; i++)
+		for (int i = 1; i < num_of_nodes + 1; i++)
 		{
 			if (visited[i] == 0)
 			{
-				if (0==k)
+				if (0 == k)
 				{
-					dfs(i, visited, nodes, 1, &max_height, &current_deepest_nodes);
+					dfs(i, visited, 1, &max_height, &current_deepest_nodes);
 				}
 				else
 				{
-					dfs(i, visited, nodes);
+					dfs(i, visited);
 				}
 				++k;
 			}
@@ -110,7 +276,7 @@ int resolve1021::resolve()
 				deepest_nodes.insert(current_deepest_nodes[j]);
 			}
 			fill(visited, visited + num_of_nodes + 1, 0);
-			dfs(*(deepest_nodes.begin()), visited, nodes, 1, &max_height, &current_deepest_nodes);
+			dfs(current_deepest_nodes[0], visited, 1, &max_height, &current_deepest_nodes);
 			for (size_t j = 0; j < current_deepest_nodes.size(); ++j)
 			{
 				deepest_nodes.insert(current_deepest_nodes[j]);
@@ -134,19 +300,19 @@ int resolve1021::resolve()
 	return 0;
 }
 
-void resolve1021::dfs(int root, int * visited, vector<vector<int>> nodes)
+void resolve1021::dfs(int root, int * visited)
 {
 	visited[root] = 1;
 	for (int i = 0; i < nodes[root].size(); ++i)
 	{
 		if (visited[nodes[root][i]] == 0)
 		{
-			dfs(nodes[root][i], visited, nodes);
+			dfs(nodes[root][i], visited);
 		}
 	}
 }
 
-void resolve1021::dfs(int root, int* visited, vector<vector<int>> nodes, int height, int* max_height, vector<int>* deepest_nodes)
+void resolve1021::dfs(int root, int* visited, int height, int* max_height, vector<int>* deepest_nodes)
 {
 	visited[root] = 1;
 	if (height > (*max_height))
@@ -163,7 +329,7 @@ void resolve1021::dfs(int root, int* visited, vector<vector<int>> nodes, int hei
 	{
 		if (visited[nodes[root][i]] == 0)
 		{
-			dfs(nodes[root][i], visited, nodes, height + 1, max_height, deepest_nodes);
+			dfs(nodes[root][i], visited, height + 1, max_height, deepest_nodes);
 		}
 	}
 }
--- PAT_Advanced/resolve1021.h
+++ PAT_Advanced/resolve1021.h
@@ -1,6 +1,7 @@
 #include "common.h"
 namespace resolve1021 {
+	vector<vector<int>> nodes;
 	int resolve();
-	void dfs(int root, int* visited, vector<vector<int>> nodes);
-	void dfs(int root, int* visited, vector<vector<int>> nodes, int height, int* max_height, vector<int>* deepest_nodes);
+	void dfs(int root, int* visited);
+	void dfs(int root, int* visited, int height, int* max_height, vector<int>* deepest_nodes);
 }
