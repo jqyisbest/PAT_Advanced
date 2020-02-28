29bdc5c2bc66dfb1c644ab7e88a0c26b12265ac5
JQY <JQYisBest@foxmail.com>
2020-02-28 13:37:46 +0800

    OAC，进制转换，非常容易

--- PAT_Advanced/main.cpp
+++ PAT_Advanced/main.cpp
@@ -1,8 +1,8 @@
 #include "common.h"
-#include "resolve1024.h"
+#include "resolve1027.h"
 
 int main() {
-	int result_code = resolve1024::resolve();
+	int result_code = resolve1027::resolve();
 	cout << "\n";
 	system("pause");
 	return result_code;
---
+++ PAT_Advanced/resolve1027.h
@@ -0,0 +1,5 @@
+#include "common.h"
+namespace resolve1027 {
+	int resolve();
+	string change_from_radix_ten(long long digits_in_radix_ten);
+}
