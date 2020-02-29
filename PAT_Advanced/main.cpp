6336b07f0c02e0ba1adfe2b74e04c32644758fb8
JQY <JQYisBest@foxmail.com>
2020-02-29 09:41:31 +0800

    AC，merge/二分查找，尽量用scanf输入！！！scanf牛逼！！！

--- PAT_Advanced/main.cpp
+++ PAT_Advanced/main.cpp
@@ -1,8 +1,8 @@
 #include "common.h"
-#include "resolve1028.h"
+#include "resolve1029.h"
 
 int main() {
-	int result_code = resolve1028::resolve();
+	int result_code = resolve1029::resolve();
 	cout << "\n";
 	system("pause");
 	return result_code;
---
+++ PAT_Advanced/resolve1029.h
@@ -0,0 +1,9 @@
+#include "common.h"
+namespace resolve1029 {
+	int resolve();
+	/**
+		二分法定位元素在nums中的位置
+		例如13579 中定位 2 ，返回1;
+	*/
+	int locate_by_binary_search(long* nums,int l,int r, long val);
+}
