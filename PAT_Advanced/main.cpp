7ebc0926cdb092df981897e3ac7b9598f1867588
JQY <JQYisBest@foxmail.com>
2020-03-02 15:14:07 +0800

    OAC，图形打印，挺好玩的2333

--- PAT_Advanced/main.cpp
+++ PAT_Advanced/main.cpp
@@ -1,8 +1,8 @@
 #include "common.h"
-#include "resolve1029.h"
+#include "resolve1031.h"
 
 int main() {
-	int result_code = resolve1029::resolve();
+	int result_code = resolve1031::resolve();
 	cout << "\n";
 	system("pause");
 	return result_code;
