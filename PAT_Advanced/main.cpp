98e1b889e0be79ffddf8402e18299a4bb06b9ed1
JQY <JQYisBest@foxmail.com>
2020-03-04 16:39:57 +0800

    OAC，贪心，艰难的很，一直没想明白该怎样贪心，测试用例都过不了==

--- PAT_Advanced/main.cpp
+++ PAT_Advanced/main.cpp
@@ -1,8 +1,8 @@
 #include "common.h"
-#include "resolve1032.h"
+#include "resolve1033.h"
 
 int main() {
-	int result_code = resolve1032::resolve();
+	int result_code = resolve1033::resolve();
 	cout << "\n";
 	system("pause");
 	return result_code;
