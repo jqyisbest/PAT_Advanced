eeefcc66c296efe85cb53674d63585c87cd70986
JQY <JQYisBest@foxmail.com>
2020-03-09 12:29:05 +0800

    AC，最长回文子串，暴力破解==；

--- PAT_Advanced/main.cpp
+++ PAT_Advanced/main.cpp
@@ -1,8 +1,8 @@
 #include "common.h"
-#include "resolve1039.h"
+#include "resolve1040.h"
 
 int main() {
-	int result_code = resolve1039::resolve();
+	int result_code = resolve1040::resolve();
 	cout << "\n";
 	system("pause");
 	return result_code;
