c97d440a0149835538aa9c4db1fcafb8edf7d134
JQY <JQYisBest@foxmail.com>
2020-03-25 12:53:10 +0800

    AC，最长不降子序列/动态规划，妈的没学过就是吃亏，建议重做。

--- PAT_Advanced/main.cpp
+++ PAT_Advanced/main.cpp
@@ -1,8 +1,8 @@
 #include "common.h"
-#include "resolve1044.h"
+#include "resolve1045.h"
 
 int main() {
-	int result_code = resolve1044::resolve();
+	int result_code = resolve1045::resolve();
 	cout << "\n";
 	system("pause");
 	return result_code;
---
+++ PAT_Advanced/resolve1045.h
@@ -0,0 +1,8 @@
+#include "common.h"
+namespace resolve1045 {
+	int resolve();
+	typedef struct stripe_node {
+		int color;
+		int length;
+	}stripe_node;
+}
