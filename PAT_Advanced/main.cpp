b449b9bcdcd9c501dcc6d268e8b28e68a59b4044
JQY <JQYisBest@foxmail.com>
2020-03-30 16:15:12 +0800

    AC，字符hash，数组还是都手动fill的好==

--- PAT_Advanced/main.cpp
+++ PAT_Advanced/main.cpp
@@ -1,8 +1,8 @@
 #include "common.h"
-#include "resolve1049.h"
+#include "resolve1050.h"
 
 int main() {
-	int result_code = resolve1049::resolve();
+	int result_code = resolve1050::resolve();
 	cout << "\n";
 	system("pause");
 	return result_code;
