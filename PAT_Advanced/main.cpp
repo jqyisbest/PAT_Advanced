7045d57b51b7c589bf80c0df4d2762b1db51e225
JQY <JQYisBest@foxmail.com>
2020-04-07 14:05:06 +0800

    OAC，树的类层序遍历，o(*￣▽￣*)ブ

--- PAT_Advanced/main.cpp
+++ PAT_Advanced/main.cpp
@@ -1,8 +1,8 @@
 #include "common.h"
-#include "resolve1052.h"
+#include "resolve1053.h"
 
 int main() {
-	int result_code = resolve1052::resolve();
+	int result_code = resolve1053::resolve();
 	cout << "\n";
 	system("pause");
 	return result_code;
--- PAT_Advanced/resolve1053.h
+++ PAT_Advanced/resolve1053.h
@@ -1,6 +1,5 @@
 #include "common.h"
 namespace resolve1053 {
 	int resolve();
-    long long *weights = nullptr;
-    bool cmp_path(vector<int>& path_1,vector<int>& path_2);
+	bool cmp_path(vector<int>& path_1, vector<int>& path_2);
 }
