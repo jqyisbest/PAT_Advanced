3091d44d0f58dcff316de5cd0a0c40b2f6e1391a
JQY <JQYisBest@foxmail.com>
2020-04-15 17:16:44 +0800

    待完成，转至VSCode处理。

--- PAT_Advanced/main.cpp
+++ PAT_Advanced/main.cpp
@@ -1,7 +1,7 @@
-#include "resolve1063.h"
+#include "resolve1064.h"
 
 int main() {
-	int result_code = resolve1063::resolve();
+	int result_code = resolve1064::resolve();
 	cout << "\n";
 	system("pause");
 	return result_code;
---
+++ PAT_Advanced/resolve1064.h
@@ -0,0 +1,5 @@
+#include "common.h"
+namespace resolve1064 {
+	int resolve();
+	int get_BST_root(int start, int end, int height);
+}
